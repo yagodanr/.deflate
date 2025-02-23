d49ac203d16cc14e5e48621b594ec3d7d48e1fca
yagodanr <yagodanr@gmail.com>
2025-02-23 13:40:30 +0200

    LZ77 kinda done, but has additional info IDK where came from

--- main.cpp
+++ main.cpp
@@ -3,10 +3,70 @@
 #include <set>
 #include <fstream>
 #include <vector>
-#include <cstring>
+#include <tuple>
+#include <list>
+
 
 using namespace std;
+#define MAX_BUFFER_SIZE 5
 
+template<typename T>
+vector<tuple<int, int, T>> LZ77(const T inp[], const int size) {
+    vector<tuple<int, int, T>> code;
+
+    list<T> buffer;
+    int buffer_size = 0;
+
+    for(int i=0; i<size;) {
+
+        int p_index = 0;
+        int p_size = 0;
+        T p_next = inp[i];
+
+        int cur_index = 0;
+        for(auto it=buffer.end(); it!=buffer.begin(); --it, ++cur_index) {
+            if(it == buffer.end()) {
+                continue;
+            }
+
+            int cur = i;
+            int cur_size = 0;
+            T cur_next = inp[cur];
+
+            auto t = it;
+            while(cur < size) {
+                if(t == buffer.end()) {
+                    t = it;
+                }
+                if(*t != cur_next) {
+                    break;
+                }
+                ++cur_size;
+                ++cur;
+                cur_next = inp[cur];
+                t++;
+            }
+
+            if(cur_size > p_size) {
+                p_index = cur_index;
+                p_size = cur_size;
+                p_next = cur_next;
+            }
+        }
+        for(int j=0; j<p_size+1; ++j, ++i) {
+            buffer.push_back(inp[i]);
+            if(buffer_size == MAX_BUFFER_SIZE) {
+                buffer.pop_front();
+            }
+            else {
+                ++buffer_size;
+            }
+        }
+        code.push_back({p_index, p_size, p_next});
+    }
+
+    return code;
+}
 
 
 int main(int argc, char *argv[]) {
@@ -16,7 +76,7 @@ int main(int argc, char *argv[]) {
     }
 
     ifstream fin;
-    fin.open(argv[1]);
+    fin.open(argv[1], ios::binary);
     if(!fin.is_open()) {
         cout << "Couldn't open the file" << endl;
         return 1;
@@ -33,10 +93,10 @@ int main(int argc, char *argv[]) {
 
         cout << "Reading " << length << " characters... ";
         // read data as a block:
-        fin.read (buffer,length);
+        fin.read(buffer,length);
 
         if (fin)
-        cout << "all characters read successfully." << endl;
+            cout << "all " << length << " characters read successfully." << endl;
         else {
             cout << "error: only " << fin.gcount() << " could be read" << endl;
             fin.close();
@@ -46,7 +106,12 @@ int main(int argc, char *argv[]) {
         }
         fin.close();
 
-        cout << buffer;
+        // cout << buffer;
+        cout << buffer << endl;
+        auto code = LZ77(buffer, length);
+        for(const auto &x: code) {
+            cout << get<0>(x) << " " << get<1>(x) << " " << get<2>(x) << endl;
+        }
 
         delete[] buffer;
     }
